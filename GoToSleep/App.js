b96e6f7b087f6e4594348b926efb79043f2e30f6
Clifford Morin <cemorin21@gmail.com>
2021-12-10 02:23:43 -0500

    press app

--- GoToSleep/App.js
+++ GoToSleep/App.js
@@ -6,107 +6,70 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, { useState } from 'react';
+import { ImageBackground, Pressable, StyleSheet, Text, View } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+	const [ timesPressed, setTimesPressed ] = useState(0);
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+	let textLog = '';
+	if (timesPressed > 1) {
+		textLog = timesPressed + 'x onPress';
+	} else if (timesPressed > 0) {
+		textLog = 'onPress';
+	}
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+	var randomColor = Math.floor(Math.random() * 16777215).toString(16);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+	return (
+		<View style={styles.container}>
+			<ImageBackground
+				style={({ pressed }) => [
+					{
+						backgroundColor: pressed ? '#' + randomColor : 'white'
+					}
+				]}
+			>
+				<Pressable
+					onPress={() => {
+						setTimesPressed((current) => current + 1);
+					}}
+					style={({ pressed }) => [
+						{
+							backgroundColor: pressed ? 'rgb(210, 230, 255)' : 'white'
+						},
+						styles.wrapperCustom
+					]}
+				>
+					{({ pressed }) => <Text style={styles.text}>{pressed ? 'Pressed!' : 'Press Me'}</Text>}
+				</Pressable>
+				<View style={styles.logBox}>
+					<Text testID="pressable_press_console">{textLog}</Text>
+				</View>
+			</ImageBackground>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'center'
+	},
+	text: {
+		fontSize: 16
+	},
+	wrapperCustom: {
+		borderRadius: 8,
+		padding: 6
+	},
+	logBox: {
+		padding: 20,
+		margin: 10,
+		borderWidth: StyleSheet.hairlineWidth,
+		borderColor: '#f09890',
+		backgroundColor: '#f98973'
+	}
 });
 
 export default App;
